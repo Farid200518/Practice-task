4c3183d1684597c9fb323751b595002e604ba152
Farid <farid.fv1@gmail.com>
2024-02-22 00:36:29 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((res) => setPosts(res.data))
+            .catch((err) => {
+                console.error('Error fetching posts', err);
+            });
+    }, []);
+
+    return (
+        <div>
+            {posts.map(post => (
+                <div key={post.id}>
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
 
 export default App;
