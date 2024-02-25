9f764f8a72e3308bd7547f6c70d14cab7274005c
Farid <farid.fv1@gmail.com>
2024-02-25 14:09:04 +0400

    a

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const App = () => {
-    const [posts, setPosts] = useState([]);
+
+    const [getData, setGetData] = useState({})
+
+
+
+    const dataToPost = {
+        id: 1,
+        title: 'Hello World',
+        body: 'Something.....'
+    }
+
+    const dataToPut = {
+        id: 2,
+        title: 'Smth...',
+        body: 'Lorem'
+    }
+
+
+    const dataToPatch = {
+        title: 'Smth...'
+    }
+
+
+
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then((res) => setPosts(res.data))
+        axios.post('https://jsonplaceholder.typicode.com/posts', dataToPost)
+            .then(response => {
+                console.log('Data created successfully:', response.data);
+            })
+            .catch(error => {
+                console.error('Error creating data:', error);
+            });
+    }, [])
+
+
+
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
+            .then((res) => setGetData(res.data))
             .catch((err) => {
                 console.error('Error fetching posts', err);
             });
     }, []);
 
+
+
+    useEffect(() => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/1')
+            .then((res) => console.log('Successfully deleted data', res.data))
+            .catch((err) => console.log('Error', err))
+    }, [])
+
+
+    useEffect(() => {
+        axios.put('https://jsonplaceholder.typicode.com/posts/2', dataToPut)
+            .then((res) => console.log('Successfully updated data', res.data))
+            .catch((err) => console.log('Error', err))
+    }, [])
+
+
+    useEffect(() => {
+        axios.patch('https://jsonplaceholder.typicode.com/posts/3', dataToPatch)
+            .then((res) => console.log('Successfully patched data', res.data))
+            .catch((err) => console.log('Error', err))
+    }, [])
+
+
+
     return (
-        <div>
-            {posts.map(post => (
-                <div key={post.id}>
-                    <h3>{post.title}</h3>
-                    <p>{post.body}</p>
+        <>
+            <div>
+                <div>
+                    <h3>{getData.title}</h3>
+                    <p>{getData.body}</p>
                 </div>
-            ))}
-        </div>
+            </div>
+
+
+        </>
     );
 };
 
